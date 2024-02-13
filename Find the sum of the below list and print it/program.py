0b5559cd497b1bcc36dd25454d6fe723652764da
KARTHI2710 <92161117+KARTHI2710@users.noreply.github.com>
2024-02-13 21:47:16 +0530

    Update program.py

--- Find the sum of the below list and print it/program.py
+++ Find the sum of the below list and print it/program.py
@@ -4,4 +4,11 @@ sum=0
 for x in numbers:
     sum=sum+x
 #print the sum value    
-print(sum)    
+print(sum)
+
+#format() function
+a=10
+b=15
+c=20
+d='karthi'
+print("I am {} , and the 3 numbers are {} , {} and {}".format(d,a,b,c))
